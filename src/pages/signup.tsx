651e1b2569472300e9881ee7e5ecf22735fab330
hananAzeez <hanan.azeez112@gmail.com>
2024-01-10 00:43:35 +0530

    signup page ui

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
 /* eslint-disable @next/next/no-img-element */
-const Login = () => {
+const Signup = () => {
   return (
     <main>
       <div className="grid lg:grid-cols-2 h-screen overflow-hidden">
         <div className="h-full w-full flex items-center justify-center px-5 lg:px-0">
           <div className="flex flex-col lg:w-2/3 xl:w-3/5 mx-auto">
-            <h1 className="text-4xl font-bold text-dark">Welcome back!</h1>
-            <h3 className="mt-4 text-xl font-medium text-light">
+            <h1 className="text-4xl font-bold text-dark">Get Started Now</h1>
+            {/* <h3 className="mt-4 text-xl font-medium text-light">
               Get ready to venture
-            </h3>
+            </h3> */}
             <form className="mt-8">
               <input
                 type="email"
@@ -18,17 +18,22 @@ const Login = () => {
                 className="rounded-xl p-4 border border-light border-opacity-60 w-full focus:outline-green"
               />
               <input
+                type="text"
+                placeholder="Phone Number"
+                className="mt-4 rounded-xl p-4 border border-light border-opacity-60 w-full focus:outline-green"
+              />
+              <input
                 type="password"
                 placeholder="Password"
                 className="mt-4 rounded-xl p-4 border border-light border-opacity-60 w-full focus:outline-green"
               />
               <button className="mt-8 text-white font-semibold rounded-xl text-lg p-5 w-full bg-green">
-                Login
+                Signup
               </button>
             </form>
             <p className="text-center mt-6 text-lg font-medium">
               Don&apos;t have an account?{" "}
-              <Link href="/signup" className="text-blue-500 hover:underline">
+              <Link href="#" className="text-blue-500 hover:underline">
                 Sign Up
               </Link>
             </p>
@@ -36,7 +41,7 @@ const Login = () => {
         </div>
         <div className="w-full h-full">
           <img
-            src="img/loginImg.jpg"
+            src="img/signupImg.jpg"
             alt="hiking"
             className="w-full h-full object-cover"
           />
@@ -46,4 +51,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
