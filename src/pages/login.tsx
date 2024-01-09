451ea3f4a9abc9cd016ff21d5d680dbc28269e3e
hananAzeez <hanan.azeez112@gmail.com>
2024-01-09 23:56:18 +0530

    login page ui done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 /* eslint-disable @next/next/no-img-element */
 const Login = () => {
   return (
     <main>
       <div className="grid lg:grid-cols-2 h-screen overflow-hidden">
         <div className="h-full w-full flex items-center justify-center px-5 lg:px-0">
-          <div className="flex flex-col lg:w-2/3 xl:w-1/2 mx-auto">
+          <div className="flex flex-col lg:w-2/3 xl:w-3/5 mx-auto">
             <h1 className="text-4xl font-bold text-dark">Welcome back!</h1>
             <h3 className="mt-4 text-xl font-medium text-light">
               Get ready to venture
@@ -24,6 +26,12 @@ const Login = () => {
                 Login
               </button>
             </form>
+            <p className="text-center mt-6 text-lg font-medium">
+              Don&apos;t have an account?{" "}
+              <Link href="#" className="text-blue-500 hover:underline">
+                Sign Up
+              </Link>
+            </p>
           </div>
         </div>
         <div className="w-full h-full">
