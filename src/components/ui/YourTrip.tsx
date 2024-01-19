2a286b90aa3a8bd8df612342ab4f567fe6951485
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-01-19 21:39:22 +0530

    yourtrip  push

--- src/components/ui/YourTrip.tsx
+++ src/components/ui/YourTrip.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+const data = [
+  {
+    title: "Delhi",
+    peopleCount: 2,
+  },
+  {
+    title: "Munnar",
+    peopleCount: 1,
+  },
+  {
+    title: "Agra",
+    peopleCount: 7,
+  },
+];
+
 const YourTrip = () => {
   return (
     <section>
-      <div className="flex items-center gap-[10px] font-[500] text-[32px] ml-20 my-10">
+      <div className="flex items-center gap-[10px] font-[500] text-[32px] ml-20 my-10 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="36"
@@ -21,6 +36,82 @@ const YourTrip = () => {
           />
         </svg>
         <p>Your Trip</p>
+      </div>
+      <div className="mt-14 ml-48">
+        <div className="flex gap-10 ">
+          {data.map((el, i) => (
+            <div
+              className="flex gap-2 flex-col rounded-xl bg-[#ffffff99]	shadow-md text-primary p-3 cursor-pointer"
+              key={i}
+            >
+              <img src={`/img/place/${i + 1}.png`} alt={`img ${el}`} />
+              <div>
+                <h3 className="text-base font-medium">{el.title}</h3>
+                <p className="text-xs font-light flex gap-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.3737 3.1202H10.2037V2.5352C10.2037 2.38004 10.1421 2.23125 10.0324 2.12154C9.92266 2.01183 9.77386 1.9502 9.61871 1.9502C9.46356 1.9502 9.31476 2.01183 9.20505 2.12154C9.09535 2.23125 9.03371 2.38004 9.03371 2.5352V3.1202H5.52371V2.5352C5.52371 2.38004 5.46208 2.23125 5.35237 2.12154C5.24266 2.01183 5.09386 1.9502 4.93871 1.9502C4.78356 1.9502 4.63476 2.01183 4.52505 2.12154C4.41534 2.23125 4.35371 2.38004 4.35371 2.5352V3.1202H3.18371C2.71826 3.1202 2.27187 3.3051 1.94274 3.63422C1.61361 3.96335 1.42871 4.40974 1.42871 4.8752V11.8952C1.42871 12.3607 1.61361 12.807 1.94274 13.1362C2.27187 13.4653 2.71826 13.6502 3.18371 13.6502H11.3737C11.8392 13.6502 12.2856 13.4653 12.6147 13.1362C12.9438 12.807 13.1287 12.3607 13.1287 11.8952V4.8752C13.1287 4.40974 12.9438 3.96335 12.6147 3.63422C12.2856 3.3051 11.8392 3.1202 11.3737 3.1202ZM11.9587 11.8952C11.9587 12.0503 11.8971 12.1991 11.7874 12.3089C11.6777 12.4186 11.5289 12.4802 11.3737 12.4802H3.18371C3.02856 12.4802 2.87976 12.4186 2.77005 12.3089C2.66034 12.1991 2.59871 12.0503 2.59871 11.8952V7.8002H11.9587V11.8952ZM11.9587 6.6302H2.59871V4.8752C2.59871 4.72004 2.66034 4.57125 2.77005 4.46154C2.87976 4.35183 3.02856 4.2902 3.18371 4.2902H4.35371V4.8752C4.35371 5.03035 4.41534 5.17914 4.52505 5.28885C4.63476 5.39856 4.78356 5.4602 4.93871 5.4602C5.09386 5.4602 5.24266 5.39856 5.35237 5.28885C5.46208 5.17914 5.52371 5.03035 5.52371 4.8752V4.2902H9.03371V4.8752C9.03371 5.03035 9.09535 5.17914 9.20505 5.28885C9.31476 5.39856 9.46356 5.4602 9.61871 5.4602C9.77386 5.4602 9.92266 5.39856 10.0324 5.28885C10.1421 5.17914 10.2037 5.03035 10.2037 4.8752V4.2902H11.3737C11.5289 4.2902 11.6777 4.35183 11.7874 4.46154C11.8971 4.57125 11.9587 4.72004 11.9587 4.8752V6.6302Z"
+                      fill="#023F76"
+                    />
+                  </svg>
+                  &nbsp;
+                  {el.peopleCount} Days
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="bg-[#ffffff99] shadow-md h-[226px] w-[180px] rounded-xl mt-12 cursor-pointer">
+          <div className="flex justify-center items-center h-full flex-col gap-[10px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="62"
+              height="62"
+              // className="my-auto"
+              viewBox="0 0 62 62"
+              fill="none"
+            >
+              <g filter="url(#filter0_b_220_918)">
+                <path
+                  d="M31.0398 0.55957C23.0498 0.656518 15.4143 3.87363 9.76409 9.52386C4.11387 15.1741 0.896752 22.8095 0.799805 30.7996C0.896752 38.7896 4.11387 46.4251 9.76409 52.0753C15.4143 57.7255 23.0498 60.9426 31.0398 61.0396C39.0298 60.9426 46.6653 57.7255 52.3155 52.0753C57.9657 46.4251 61.1829 38.7896 61.2798 30.7996C61.1829 22.8095 57.9657 15.1741 52.3155 9.52386C46.6653 3.87363 39.0298 0.656518 31.0398 0.55957ZM48.3198 32.9596H33.1998V48.0796H28.8798V32.9596H13.7598V28.6396H28.8798V13.5196H33.1998V28.6396H48.3198V32.9596Z"
+                  fill="#023F76"
+                />
+              </g>
+              <defs>
+                <filter
+                  id="filter0_b_220_918"
+                  x="-3.5202"
+                  y="-3.76043"
+                  width="69.1205"
+                  height="69.1205"
+                  filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB"
+                >
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feGaussianBlur in="BackgroundImageFix" stdDeviation="2.16" />
+                  <feComposite
+                    in2="SourceAlpha"
+                    operator="in"
+                    result="effect1_backgroundBlur_220_918"
+                  />
+                  <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="effect1_backgroundBlur_220_918"
+                    result="shape"
+                  />
+                </filter>
+              </defs>
+            </svg>
+            <p className="text-[#023F76] text-[18px] font-[500] ">New Trip</p>
+          </div>
+        </div>
       </div>
     </section>
   );
